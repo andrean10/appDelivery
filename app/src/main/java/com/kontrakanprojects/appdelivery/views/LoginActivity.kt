afabc9e90bbcc7da0bb5636f755e7381847b43e9
Suzukaze1 <alvinmd68@gmail.com>
2021-04-30 15:27:17 +0700

    Update the frontend . Login . Profile . Public (Where to see our tracking) . DetailBarang (Where to see what is your barang)

---
+++ app/src/main/java/com/kontrakanprojects/appdelivery/views/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.kontrakanprojects.appdelivery.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kontrakanprojects.appdelivery.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}

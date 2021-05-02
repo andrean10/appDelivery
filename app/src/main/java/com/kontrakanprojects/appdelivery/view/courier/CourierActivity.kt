2cfd03c0c2e8c437c861c4fdf71988c70939304d
andrean10 <andreanramadhan10@gmail.com>
2021-05-02 16:31:52 +0700

    Menambahkan routing navigation components

---
+++ app/src/main/java/com/kontrakanprojects/appdelivery/view/courier/CourierActivity.kt
@@ -0,0 +1,12 @@
+package com.kontrakanprojects.appdelivery.view.courier
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kontrakanprojects.appdelivery.R
+
+class CourierActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_courier)
+    }
+}

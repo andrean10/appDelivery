dcfdae8a044a4e05aecf5aa92bb04e885cbe1d12
Suzukaze1 <alvinmd68@gmail.com>
2021-06-14 23:47:18 +0700

    small add : tab layout and rv tab1 and tab2 -Aru

--- app/src/main/java/com/kontrakanprojects/appdelivery/view/admin/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/kontrakanprojects/appdelivery/view/admin/dashboard/DashboardFragment.kt
@@ -111,7 +111,7 @@ class DashboardFragment : Fragment(), View.OnClickListener {
                 }
             }
             R.id.btn_courier -> {
-                findNavController().navigate(R.id.action_dashboardFragment_to_barangKurirFragment)
+                findNavController().navigate(R.id.action_dashboardFragment_to_listCouriersFragment)
             }
             R.id.btn_tracking -> {
                 findNavController().navigate(R.id.action_dashboardFragment_to_trackingBarangFragment)

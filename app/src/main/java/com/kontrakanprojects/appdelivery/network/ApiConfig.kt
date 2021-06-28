87bb14dc1d823cdc19840545d01bb795d4698191
putriinsaniahnur <Putriinsaninrr@gmail.com>
2021-06-28 15:17:43 +0700

    add: backup hosting -aru

--- app/src/main/java/com/kontrakanprojects/appdelivery/network/ApiConfig.kt
+++ app/src/main/java/com/kontrakanprojects/appdelivery/network/ApiConfig.kt
@@ -8,7 +8,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 class ApiConfig {
     companion object {
         // hosting
-        const val URL = "http://192.168.100.8:8000"
+        //const val URL = "http://delivery.rproject-dev.com"
+        // hosting backup
+        const val URL = "https://aruvin.000webhostapp.com"
+        // local
+        // const val URL = "http://192.168.43.24:8000"
         const val IMG_URL = "$URL/public"
 
         private const val ENDPOINT = "$URL/api/"
